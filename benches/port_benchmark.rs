fcdbf1a1c90cef905e5cc14f772bd8440abc82d2
Markus Mayer <widemeadows@gmail.com>
2021-07-25 16:35:04 +0200

    Add fully const generic implementation

--- benches/port_benchmark.rs
+++ benches/port_benchmark.rs
@@ -1,25 +1,35 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use moore_neighborhood::{moore, moore_dynamic};
+use moore_neighborhood::{dynamic, generic_dimension, generic_full};
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("ported d=3, r=1", |b| {
-        b.iter(|| moore_dynamic(black_box(1), black_box(3)))
+        b.iter(|| dynamic::moore(black_box(1), black_box(3)))
     });
     c.bench_function("ported d=2, r=1", |b| {
-        b.iter(|| moore_dynamic(black_box(1), black_box(2)))
+        b.iter(|| dynamic::moore(black_box(1), black_box(2)))
     });
     c.bench_function("ported d=2, r=2", |b| {
-        b.iter(|| moore_dynamic(black_box(2), black_box(2)))
+        b.iter(|| dynamic::moore(black_box(2), black_box(2)))
     });
 
     c.bench_function("ported generic d=3, r=1", |b| {
-        b.iter(|| moore::<3>(black_box(1)))
+        b.iter(|| generic_dimension::moore::<3>(black_box(1)))
     });
     c.bench_function("ported generic d=2, r=1", |b| {
-        b.iter(|| moore::<2>(black_box(1)))
+        b.iter(|| generic_dimension::moore::<2>(black_box(1)))
     });
     c.bench_function("ported generic d=2, r=2", |b| {
-        b.iter(|| moore::<2>(black_box(2)))
+        b.iter(|| generic_dimension::moore::<2>(black_box(2)))
+    });
+
+    c.bench_function("ported full generic d=3, r=1", |b| {
+        b.iter(|| generic_full::moore::<1, 3, 26>())
+    });
+    c.bench_function("ported full generic d=2, r=1", |b| {
+        b.iter(|| generic_full::moore::<1, 2, 8>())
+    });
+    c.bench_function("ported full generic d=2, r=2", |b| {
+        b.iter(|| generic_full::moore::<2, 2, 24>())
     });
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,96 @@
-pub fn moore_dynamic(range: u32, dimensions: u32) -> Vec<Vec<isize>> {
-    let size: usize = range as usize * 2 + 1;
-    let length: usize = size.pow(dimensions) - 1;
-    let half_length = length / 2;
-    let mut neighbors = Vec::with_capacity(length as _);
+pub mod dynamic {
+    pub fn moore(range: u32, dimensions: u32) -> Vec<Vec<isize>> {
+        let size: usize = range as usize * 2 + 1;
+        let length: usize = size.pow(dimensions) - 1;
+        let half_length = length / 2;
+        let mut neighbors = Vec::with_capacity(length as _);
 
-    for i in 0usize..length {
-        let mut neighbor = Vec::with_capacity(dimensions as _);
-        let mut index = if i < half_length { i } else { i + 1 };
-        let mut prev_divisor = 1;
-        for _dimension in 0..dimensions {
-            let divisor = prev_divisor * size;
-            let value = index % divisor;
-            neighbor.push((value / prev_divisor) as isize - range as isize);
-            prev_divisor = divisor;
-            index -= value;
+        for i in 0usize..length {
+            let mut neighbor = Vec::with_capacity(dimensions as _);
+            let mut index = if i < half_length { i } else { i + 1 };
+            let mut prev_divisor = 1;
+            for _dimension in 0..dimensions {
+                let divisor = prev_divisor * size;
+                let value = index % divisor;
+                neighbor.push((value / prev_divisor) as isize - range as isize);
+                prev_divisor = divisor;
+                index -= value;
+            }
+
+            neighbors.push(neighbor);
         }
-
-        neighbors.push(neighbor);
+        neighbors
     }
-    neighbors
 }
 
-pub fn moore<const DIMENSIONS: usize>(range: u32) -> Vec<[isize; DIMENSIONS]> {
-    assert!(DIMENSIONS < u32::MAX as _);
+pub mod generic_dimension {
+    pub fn moore<const DIMENSIONS: usize>(range: u32) -> Vec<[isize; DIMENSIONS]> {
+        assert!(DIMENSIONS < u32::MAX as _);
 
-    let size: usize = range as usize * 2 + 1;
-    let length: usize = size.pow(DIMENSIONS as _) - 1;
-    let half_length = length / 2;
-    let mut neighbors = Vec::with_capacity(length as _);
+        let size: usize = range as usize * 2 + 1;
+        let length: usize = size.pow(DIMENSIONS as _) - 1;
+        let half_length = length / 2;
+        let mut neighbors = Vec::with_capacity(length as _);
 
-    for i in 0usize..length {
-        let mut neighbor = [0; DIMENSIONS];
-        let mut index = if i < half_length { i } else { i + 1 };
-        let mut prev_divisor = 1;
-        for dimension in 0..DIMENSIONS {
-            let divisor = prev_divisor * size;
-            let value = index % divisor;
-            neighbor[dimension] = (value / prev_divisor) as isize - range as isize;
-            prev_divisor = divisor;
-            index -= value;
+        for i in 0usize..length {
+            let mut neighbor = [0; DIMENSIONS];
+            let mut index = if i < half_length { i } else { i + 1 };
+            let mut prev_divisor = 1;
+            for dimension in 0..DIMENSIONS {
+                let divisor = prev_divisor * size;
+                let value = index % divisor;
+                neighbor[dimension] = (value / prev_divisor) as isize - range as isize;
+                prev_divisor = divisor;
+                index -= value;
+            }
+
+            neighbors.push(neighbor);
+        }
+        neighbors
+    }
+}
+
+pub mod generic_full {
+    #[inline]
+    pub fn moore<const RANGE: u32, const DIMENSIONS: usize, const LENGTH: usize>(
+    ) -> [[isize; DIMENSIONS]; LENGTH] {
+        assert!(DIMENSIONS < u32::MAX as _);
+
+        {
+            let size: usize = RANGE as usize * 2 + 1;
+            debug_assert_eq!(LENGTH, size.pow(DIMENSIONS as _) - 1);
         }
 
-        neighbors.push(neighbor);
+        let mut neighbors = [[0isize; DIMENSIONS]; LENGTH];
+        moore_prealloc::<RANGE, DIMENSIONS, LENGTH>(&mut neighbors);
+        neighbors
     }
-    neighbors
+
+    pub fn moore_prealloc<const RANGE: u32, const DIMENSIONS: usize, const LENGTH: usize>(
+        neighbors: &mut [[isize; DIMENSIONS]; LENGTH],
+    ) -> usize {
+        assert!(DIMENSIONS < u32::MAX as _);
+
+        let size: usize = RANGE as usize * 2 + 1;
+        let length = size.pow(DIMENSIONS as _) - 1;
+        assert!(LENGTH >= length);
+
+        let half_length = LENGTH / 2;
+        for i in 0usize..LENGTH {
+            let neighbor = &mut neighbors[i];
+
+            let mut index = if i < half_length { i } else { i + 1 };
+            let mut prev_divisor = 1;
+            for dimension in 0..DIMENSIONS {
+                let divisor = prev_divisor * size;
+                let value = index % divisor;
+                neighbor[dimension] = (value / prev_divisor) as isize - RANGE as isize;
+                prev_divisor = divisor;
+                index -= value;
+            }
+        }
+        length
+    }
 }
 
 #[cfg(test)]
@@ -52,7 +99,7 @@ mod tests {
 
     #[test]
     fn dyn_d2_r1_works() {
-        let mut result = moore_dynamic(1, 2);
+        let mut result = dynamic::moore(1, 2);
 
         #[rustfmt::skip]
         let mut expected = [
@@ -68,7 +115,23 @@ mod tests {
 
     #[test]
     fn gen_dim_d2_r1_works() {
-        let mut result: Vec<[isize; 2]> = moore(1);
+        let mut result: Vec<[isize; 2]> = generic_dimension::moore(1);
+
+        #[rustfmt::skip]
+        let mut expected = [
+            [-1,-1], [ 0,-1], [ 1,-1],
+            [-1, 0],          [ 1, 0],
+            [-1, 1], [ 0, 1], [ 1, 1]
+        ];
+
+        result.sort();
+        expected.sort();
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn gen_x_d2_r1_works() {
+        let mut result: [[isize; 2]; 8] = generic_full::moore::<1, 2, 8>();
 
         #[rustfmt::skip]
         let mut expected = [
@@ -84,7 +147,7 @@ mod tests {
 
     #[test]
     fn dyn_d2_r2_works() {
-        let mut result = moore_dynamic(2, 2);
+        let mut result = dynamic::moore(2, 2);
 
         #[rustfmt::skip]
         let mut expected = [
@@ -102,7 +165,7 @@ mod tests {
 
     #[test]
     fn gen_dim_d2_r2_works() {
-        let mut result: Vec<[isize; 2]> = moore(2);
+        let mut result: Vec<[isize; 2]> = generic_dimension::moore(2);
 
         #[rustfmt::skip]
         let mut expected = [
@@ -120,7 +183,7 @@ mod tests {
 
     #[test]
     fn dyn_d3_r1_works() {
-        let mut result = moore_dynamic(1, 3);
+        let mut result = dynamic::moore(1, 3);
 
         #[rustfmt::skip]
         let mut expected = [
@@ -144,7 +207,7 @@ mod tests {
 
     #[test]
     fn dyn_same_as_reference() {
-        let result = moore_dynamic(3, 3);
+        let result = dynamic::moore(3, 3);
         let expected = reference(3, 3);
         assert_eq!(result, expected);
     }
